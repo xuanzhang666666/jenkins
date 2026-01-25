1801a52fcbfeff4269c5a4718a467edb2486d72a
xuan.zhang <v8b9764tch@privaterelay.appleid.com>
2026-01-25 11:15:16 +0800

    1

--- core/src/main/java/wormpex/data/util/Pair.java
+++ core/src/main/java/wormpex/data/util/Pair.java
@@ -39,8 +39,7 @@ public class Pair {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Pair pair = (Pair) o;
-        return Objects.equals(left, pair.left) &&
-                Objects.equals(right, pair.right);
+        return Objects.equals(left, pair.left) && Objects.equals(right, pair.right);
     }
 
     @Override
@@ -50,67 +49,6 @@ public class Pair {
 
     @Override
     public String toString() {
-        return "Pair{" +
-                "left='" + left + '\'' +
-                ", right='" + right + '\'' +
-                '}';
-    }
-}
-package wormpex.data.util;
-
-import java.util.Objects;
-
-public class Pair {
-    private String left;
-    private String right;
-
-    public Pair() {
-    }
-
-    public Pair(String left, String right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public static Pair of(String left, String right) {
-        return new Pair(left, right);
-    }
-
-    public String getLeft() {
-        return left;
-    }
-
-    public void setLeft(String left) {
-        this.left = left;
-    }
-
-    public String getRight() {
-        return right;
-    }
-
-    public void setRight(String right) {
-        this.right = right;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair pair = (Pair) o;
-        return Objects.equals(left, pair.left) &&
-                Objects.equals(right, pair.right);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(left, right);
-    }
-
-    @Override
-    public String toString() {
-        return "Pair{" +
-                "left='" + left + '\'' +
-                ", right='" + right + '\'' +
-                '}';
+        return "Pair{" + "left='" + left + '\'' + ", right='" + right + '\'' + "}";
     }
 }
--- core/src/main/java/wormpex/data/util/UpstreamTriggerChain.java
+++ core/src/main/java/wormpex/data/util/UpstreamTriggerChain.java
@@ -1,89 +1,17 @@
 package wormpex.data.util;
 
 import com.google.common.collect.Lists;
-import org.kohsuke.stapler.export.Exported;
-import org.kohsuke.stapler.export.ExportedBean;
-
-import java.util.List;
-
-public class UpstreamTriggerChain {
-
-    private String triggerUser = "KNOWN";
-    private List<JobBuildSimpleInfo> jobBuildSimpleInfos = Lists.newArrayList();
-
-    public String getTriggerUser() {
-        return triggerUser;
-    }
-
-    public void setTriggerUser(String triggerUser) {
-        this.triggerUser = triggerUser;
-    }
-
-    public List<JobBuildSimpleInfo> getJobBuildSimpleInfos() {
-        return jobBuildSimpleInfos;
-    }
-
-    public void setJobBuildSimpleInfos(List<JobBuildSimpleInfo> jobBuildSimpleInfos) {
-        this.jobBuildSimpleInfos = jobBuildSimpleInfos;
-    }
-
-    @Override
-    public String toString() {
-        return "UpstreamTriggerChain{" +
-                "triggerUser='" + triggerUser + '\'' +
-                ", jobBuildSimpleInfos=" + jobBuildSimpleInfos +
-                '}';
-    }
-
-    public static class JobBuildSimpleInfo {
-        String jobName;
-        int buildId;
-
-        public String getJobName() {
-            return jobName;
-        }
-
-        public void setJobName(String jobName) {
-            this.jobName = jobName;
-        }
-
-        public int getBuildId() {
-            return buildId;
-        }
-
-        public void setBuildId(int buildId) {
-            this.buildId = buildId;
-        }
-
-        @Override
-        public String toString() {
-            return "JobBuildSimpleInfo{" +
-                    "jobName='" + jobName + '\'' +
-                    ", buildId=" + buildId +
-                    '}';
-        }
-    }
-}
-package wormpex.data.util;
-
-import com.google.common.collect.Lists;
-import org.kohsuke.stapler.export.Exported;
-import org.kohsuke.stapler.export.ExportedBean;
-
 import java.util.List;
 
 /**
- * <br>
- * <b>功能:</b><br>
- * <b>作者:</b> yashiro <br>
- * <b>日期:</b> 2018/10/26 <br>
+ * 上游触发链路信息，记录触发者和上游构建简要信息。
  */
 public class UpstreamTriggerChain {
 
-    //第一个任务的触发者
+    // 第一个任务的触发者
     private String triggerUser = "KNOWN";
 
-    //上游链路中的触发信息，只有任务名和构建id
+    // 上游链路中的触发信息，只有任务名和构建 id
     private List<JobBuildSimpleInfo> jobBuildSimpleInfos = Lists.newArrayList();
 
     public String getTriggerUser() {
@@ -111,8 +39,8 @@ public class UpstreamTriggerChain {
     }
 
     public static class JobBuildSimpleInfo {
-        String jobName;
-        int buildId;
+        private String jobName;
+        private int buildId;
 
         public String getJobName() {
             return jobName;
@@ -138,5 +66,4 @@ public class UpstreamTriggerChain {
                     '}';
         }
     }
-
 }
